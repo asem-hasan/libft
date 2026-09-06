297e25e99ba26eed0b1962c65fcbf146d2e8adf7
Asem Hasan <jannekim076@gmail.com>
2026-09-06 23:33:12 +0300

    Added the files

---
+++ libft.h
@@ -0,0 +1,6 @@
+# ifndef LIBFT_H
+# define LIBFT_H
+
+int	ft_isalpha(char c);
+
+#endif
---
+++ test.c
@@ -0,0 +1,10 @@
+#include "libft.h"
+#include <stdio.h>
+
+int main(void)
+{
+    printf("%d\n", ft_isalpha('a'));
+    printf("%d\n", ft_isalpha('1'));
+
+    return (0);
+}
